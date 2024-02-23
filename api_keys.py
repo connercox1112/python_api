c714a8112174f588b45e441d79348ebaf4f5c465
Conner Cox <connercox1112@gmail.com>
2024-02-23 07:47:10 -0600

    Third commit

--- api_keys.py
+++ api_keys.py
@@ -1,7 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "API KEY"
+weather_api_key = "8f3ef82fa149e9d415b5ce88b23fb897"
 
 # Geoapify API Key
-geoapify_key = "API KEY"
+geoapify_key = "e2095de852704fa9896f8eb25ce3fda1"
 
-*api_keys.py
+
